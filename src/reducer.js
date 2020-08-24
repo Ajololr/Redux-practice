b9adc66ff4bcf95f238193ce476c897b7a26c169
Ilya Androsov <ilya_androsov@icloud.com>
2020-08-24 12:00:34 +0300

    feat: add store

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 let lastId = 0;
 
-function (state, action) {
+export default function reducer(state = [], action) {
   if (action.type === "bugAdded")
     return [
       ...state,
@@ -8,10 +8,10 @@ function (state, action) {
         id: ++lastId,
         description: action.payload.description,
         resolved: false,
-      }
-    ]
+      },
+    ];
   else if (action.type === "bugRemoved")
-    return state.bugs.filter(bug => bug.id !== action.payload.id)
+    return state.bugs.filter((bug) => bug.id !== action.payload.id);
 
   return state;
 }

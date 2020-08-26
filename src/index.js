017367d132c7d57eaaa00770573ecdd9ac7326c2
Ilya Androsov <ilya_androsov@icloud.com>
2020-08-26 13:28:30 +0300

    feat: add projects slice

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import configureStore from "./store/configureStore";
 import { bugAdded, bugResolved } from "./store/bugs";
+import { projectAdded } from "./store/projects";
 
 const store = configureStore();
 
-store.dispatch(bugAdded({ description: "New bug" }));
-store.dispatch(bugAdded({ description: "New bug" }));
-store.dispatch(bugAdded({ description: "New bug" }));
-store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(bugAdded({ description: "New bug" }));
+// store.dispatch(bugAdded({ description: "New bug" }));
+// store.dispatch(bugAdded({ description: "New bug" }));
+// store.dispatch(bugResolved({ id: 1 }));
+
+store.dispatch(projectAdded({ name: "My project" }));
 
 console.log(store.getState());
